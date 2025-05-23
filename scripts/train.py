170ff1d62ed26997ae070934a1f66b24cde01143
Dhananjay Tiwari <dhananjaytiwari2403@gmail.com>
2025-05-23 13:41:26 +0530

    Python script to train model

--- scripts/train.py
+++ scripts/train.py
@@ -1,6 +1,20 @@
 from ultralytics import YOLO
 
-# Load pretrained YOLOv8n model
-model = YOLO('yolov8n.pt')
+def train_model():
+    model = YOLO('yolov8n.pt')
+    model.train(
+        data='../config/data.yaml', 
+        # epochs=1,                  # initially trained model with epoch = 1 
+        # imgsz=320,                 # used reduced image size initially
+        # fraction=0.05              # Use only 5% of dataset initially
 
-model.train()
+        epochs = 25,                   # initially trained model with epoch = 1 
+        imgsz = 640,                   # image size
+        batch = 16,                  # default batch size for full training 
+        fraction = 0.5,              # Using 50% of dataset for training
+        name = 'full_training'       # custom name for O/P folder
+    )
+
+if __name__ == "__main__":
+    train_model()
+    
